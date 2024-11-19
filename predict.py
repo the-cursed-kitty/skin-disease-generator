a260ee6b6712c0243429e4f0a821d6ad7d342f07
LokheshKumar <59267877+the-cursed-kitty@users.noreply.github.com>
2024-11-19 19:30:31 +0530

    Add files via upload

--- predict.py
+++ predict.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 model = load_model('skin_lesion_classifier.h5')
 
 # List of labels based on your training data
-labels = ['Acne', 'Eczema', 'Psoriasis', 'Benign Lesion', 'Malignant Lesion', 'Allergic Reaction', 'Infection']
+labels = ['Acne and Rosacea Photos', 'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions', 'Atopic Dermatitis Photos', 'Bullous Disease Photos', 'Cellulitis Impetigo and other Bacterial Infections', 'Eczema Photos', 'Exanthems and Drug Eruptions', 'Hair Loss Photos Alopecia and other Hair Diseases', 'Herpes HPV and other STDs Photos', 'Light Diseases and Disorders of Pigmentation', 'Lupus and other Connective Tissue diseases', 'Melanoma Skin Cancer Nevi and Moles', 'Nail Fungus and other Nail Disease', 'Poison Ivy Photos and other Contact Dermatitis', 'Psoriasis pictures Lichen Planus and related diseases', 'Scabies Lyme Disease and other Infestations and Bites', 'Seborrheic Keratoses and other Benign Tumors', 'Systemic Disease', 'Tinea Ringworm Candidiasis and other Fungal Infections', 'Urticaria Hives', 'Vascular Tumors', 'Vasculitis Photos', 'Warts Molluscum and other Viral Infections']
 
 def predict_image(img_path):
     img = image.load_img(img_path, target_size=(224, 224))
